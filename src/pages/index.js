b9997a711f093dbb2a0028591ed1e118372b3296
Jon Arne S <seteras@gmail.com>
2021-11-25 20:39:11 +0100

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,34 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import { graphql } from "gatsby"
 
 export default function IndexPage({ data }) {
+  console.log("data: ", data)
   return (
     <GatsbyImage
-      image={data.file.childImageEngineAsset.url}
+      image={data.file.childImageEngineAsset.gatsbyImageData}
       alt="icon"
     />
   )
 }
+/*
+export const query = graphql`
+  query {
+    file(name: {eq: "icon"}) {
+      childImageSharp {
+        gatsbyImageData(width: 100)
+      }
+    }
+}
+`
+*/
+
 
 export const query = graphql`
   query {
     file(name: {eq: "icon"}) {
       childImageEngineAsset {
-          gatsbyImageData(width: 500, height: 300)
-          url(width: 500, height: 300, compression: 10)
+        url(width: 500, height: 300, compression: 10)
+        gatsbyImageData(width: 500, height: 300)
+
       }
     }
 }
